5a985ce615a18f1f93caf8af9aea6af02a7048a0
teSedge <teSedge99@gmail.com>
2022-06-08 16:52:02 +0300

    shop 1.0

---
+++ shop/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+	path('', views.home, name='home'),
+    path('category/<slug:category_slug>', views.home, name='products_by_category'),
+	path('category/<slug:category_slug>/<slug:product_slug>', views.product, name='product_detail'),
+	path('cart',views.cart_detail,name = 'cart_detail'),
+	path('cart/add/<int:product_id>',views.add_cart, name = 'add_cart'),
+	path('cart/remove/<int:product_id>',views.remove_cart, name = 'remove_cart'),
+	path('cart/delete/<int:product_id>',views.remove_cart, name = 'delete_cart'),
+
+]
